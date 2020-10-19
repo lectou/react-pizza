d9edeeb370c6444bc55bff829d3c764004812905
Портянко Олександр <lara_57_pr@outlook.com>
2020-10-19 19:45:12 +0300

    proxy server

--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -8,7 +8,7 @@ export const loader = (value) => ({ type: "LOADER", payload: value });
 //get pizzas thunk
 export const getPizzas = (category, sort) => (dispatch) => {
   // dispatch(loader(false));
-  Axios.get(`http://localhost:3001/pizzas?${category !== null
+  Axios.get(`/pizzas?${category !== null
     ? `category=${category}` : ""}&_sort=${sort.type}&_order=${sort.order}`)
     .then(({ data }) => {
       dispatch(setPizzas(data));
